789a33ffaae94db927a12ff2529abc7518366016
JoshArega <112011105+JoshArega@users.noreply.github.com>
2024-02-18 15:34:03 -0500

    Basic look at json files with UUID generator

---
+++ json/UUIDGen.java
@@ -0,0 +1,11 @@
+package json;
+
+import java.util.UUID;
+
+public class UUIDGen {
+    
+    public static void main(String[] args) {
+        UUID id = UUID.randomUUID();
+        System.out.println(id);
+    }
+}
