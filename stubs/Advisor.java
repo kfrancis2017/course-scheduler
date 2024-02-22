7aa6fd33dfcf56bfd79326f2d7bf387597e15c4c
JoshArega <112011105+JoshArega@users.noreply.github.com>
2024-02-22 15:11:26 -0500

    Added stubs

---
+++ stubs/Advisor.java
@@ -0,0 +1,32 @@
+package stubs;
+
+import java.util.ArrayList;
+
+public class Advisor extends User{
+    
+    private ArrayList<Student> adviseeList;
+
+    public Advisor() {
+
+    }
+
+    public AdviseeList getAdviseeList(userDB) {
+        
+    }
+
+    public void monitorProgress(String username) {
+
+    }
+
+    public void addAdvisingSession(User student) {
+
+    }
+
+    public void createSechedule(String username) {
+
+    }
+
+    public void getStudentInfo(User student) {
+
+    }
+}
---
+++ stubs/DegreeWorks.java
@@ -0,0 +1,13 @@
+package stubs;
+
+public class DegreeWorks {
+    
+    private User user;
+    private String mCourseList;
+    private String mProgramList;
+    private String mUserList;
+
+    public boolean login(String username, String password) {
+        return true;
+    }
+}
