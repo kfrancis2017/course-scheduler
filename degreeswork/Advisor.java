59e98547712f8bbc60eb0a2fd2cd1829baa9f961
j-vaught <jvaught@sc.edu>
2024-02-29 13:56:21 -0500

    cleaned advisor. issue #78

--- degreeswork/Advisor.java
+++ degreeswork/Advisor.java
@@ -15,20 +15,8 @@ public class Advisor extends User {
         return this.adviseeList;
     }
 
-    public void monitorProgress(String username) {
-
-    }
-
     public void addAdvisingSession(User student) {
         //ok
-    }
-
-    public void createSechedule(String username) {
-        //Should NOT be here.
-    }
-
-    public void getStudentInfo(User student) {
-        //what the f does this do?
     }
 
     public void setAdviseeList(ArrayList<String> advisees) {
