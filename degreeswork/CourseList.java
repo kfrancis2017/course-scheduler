934fbb5d5aaea14a8d6c08f4bb2ccc460de550fc
JoshArega <112011105+JoshArega@users.noreply.github.com>
2024-03-12 20:36:55 -0400

    More progress on CourseList

--- degreeswork/CourseList.java
+++ degreeswork/CourseList.java
@@ -1,11 +1,9 @@
 package degreeswork;
 
+import java.util.HashMap;
 import java.util.ArrayList;
 
-import java.util.HashMap;
-
 public class CourseList {
-<<<<<<< HEAD
     private static CourseList courseList;
     private ArrayList<Course> courses;
     private HashMap<String, Course> coursesByCode;
@@ -30,8 +28,8 @@ public class CourseList {
 
     /**
      * Adds a Course object to the list
-     * @param course
-     * @return
+     * @param course The Course object
+     * @return False if null, true otherwise
      */
     public boolean addCourse(Course course) {
         if (course == null)
@@ -42,57 +40,64 @@ public class CourseList {
 
     /**
      * Creates a new Course object and adds to the list
-     * @param coursename 
-     * @param courseID
-     * @param description
-     * @param prereq
-     * @param coreq
-     * @param AOS_Req
-     * @return
+     * @param coursename The name of the course
+     * @param courseID The course identifier 
+     * @param description The course description
+     * @param prereq The course prerequisites
+     * @param coreq The course corequisites
+     * @param AOS_Req The Area of Study Requirements the course falls under
+     * @return Boolean value of true
      */
     public boolean addCourse(String coursename, String courseID, String description, CourseList prereq, CourseList coreq, String AOS_Req) {
         courses.add(new Course(coursename, courseID, description, prereq, coreq, AOS_Req));
         return true;
     }
 
+    /**
+     * Replaces a old course with a new course in the list
+     * @param newCourse The new Course object
+     * @param oldCourse The old Course object
+     * @return False if new = old, True if new != old
+     */
     public boolean modifyCourse(Course newCourse, Course oldCourse) {
         if (newCourse == oldCourse)
             return false;
-        courses.remove(newCourse);
-        courses.add(oldCourse);
+        courses.remove(oldCourse);
+        courses.add(newCourse);
         return true;
 
     }
 
+    /**
+     * Removes a Course from the list
+     * @param course The Course object
+     * @return Boolean value representing the success of course deletion
+     */
     public boolean deleteCourse(Course course) {
         if (course != findCourseByCode(course.getCourseID()))
             return false;
         courses.remove(course);
         return true;
-=======
-    
-    private static HashMap<String, Course>courseHash = new HashMap<String, Course>();
-    private static ArrayList<Course> courses;
-    
-    public boolean addCourse(Course course) {
-        courseHash.put(course.courseID, course);
-        return false;
     }
 
-    public boolean modifyCourse(Course newCourse, Course oldCourse) {
-        courseHash.remove(oldCourse);
-        courseHash.put(newCourse.courseID, newCourse);
-        return false;
+    /**
+     * Utilizes the HashMap to find courses by their "key"
+     * @param courseID The "key" to access elements of the HashMap
+     * @return The desired Course
+     */
+    public Course findCourseByCode(String courseID) {
+        loadHashMap();
+        return coursesByCode.get(courseID);
     }
 
-    public boolean deleteCourse(Course course) {
-        courseHash.remove(course);
-        return false;
->>>>>>> 6b5331cad303fd6386e11a7b43d9d74df951a6d0
-    }
+    /**
+     * Transfers the ArrayList of Courses into the Hashmap
+     */
+    public void loadHashMap() {
 
-    public Course findCourse(String courseID) {
-        courseHash.get(courseID);
-        return null;
+        for (Course course : courses) {
+            coursesByCode.put(course.getCourseID(), course);
+        }
+
     }
 }
