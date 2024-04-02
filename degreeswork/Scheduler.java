6024612263228f7f63471d4de78ab197e9e0eea8
Kevin Francis <francis.kevin2016@gmail.com>
2024-04-02 12:39:40 -0400

    Fixed major.json && Attempted fix on testScheduler

--- degreeswork/Scheduler.java
+++ degreeswork/Scheduler.java
@@ -17,7 +17,9 @@ public class Scheduler {
         this.completedCourses = student.getFinishedCourseObjects(allCoursesList);
         this.allCourses = new HashMap<>();
         for (Course course : allCoursesList) {
-            this.allCourses.put(course.getCourseID(), course);
+            if (course != null) { // Check if the course is not null
+                this.allCourses.put(course.getCourseID(), course);
+            }
         }
     }
 
@@ -45,40 +47,41 @@ public class Scheduler {
         ArrayList<Course> needToTake = new ArrayList<>();
 
         for (Course course : reqCourses) {
+            if (course != null) { //Check if the course is not null
+                if (!compCourses.contains(course)) {// if completed course does NOT contain the course
+                    if (isPrerequisiteCompleted(course.getPrereq(), compCourses)) {// if all prereqs are completed
+                        needToTake.add(course);
+                    } else {// if NOT ALL prereqs are completed
+                        needToTake.add(course);
+                        for (ArrayList<String> prereqList : course.getPrereq()) {
 
-            if (!compCourses.contains(course)) {// if completed course does NOT contain the course
-                if (isPrerequisiteCompleted(course.getPrereq(), compCourses)) {// if all prereqs are completed
-                    needToTake.add(course);
-                } else {// if NOT ALL prereqs are completed
-                    needToTake.add(course);
-                    for (ArrayList<String> prereqList : course.getPrereq()) {
+                            if (prereqList.size() == 1) {// If there's only one prerequisite
 
-                        if (prereqList.size() == 1) {// If there's only one prerequisite
-
-                            String prereq = prereqList.get(0); // Get the prerequisite course ID
-                            // Check if the course hasn't been completed or already checked
-                            if (!compCourses.contains(allCourses.get(prereq))
-                                    && !needToTake.contains(allCourses.get(prereq))) {
-                                needToTake.add(allCourses.get(prereq)); // Add to the list of needed courses
-                            }
-                        } else { // If there are multiple prerequisites
-                            // Check if any of the prerequisites have been completed
-                            boolean anyPrereqCompleted = false;
-                            for (String prereq : prereqList) {
-                                if (compCourses.contains(allCourses.get(prereq))) {
-                                    anyPrereqCompleted = true;
-                                    break;
+                                String prereq = prereqList.get(0); // Get the prerequisite course ID
+                                // Check if the course hasn't been completed or already checked
+                                if (!compCourses.contains(allCourses.get(prereq))
+                                        && !needToTake.contains(allCourses.get(prereq))) {
+                                    needToTake.add(allCourses.get(prereq)); // Add to the list of needed courses
                                 }
-                            }
-                            // If none of the prerequisites have been completed
-                            if (!anyPrereqCompleted && !needToTake.contains(allCourses.get(prereqList.get(0)))) {
-                                needToTake.add(allCourses.get(prereqList.get(0))); // Add the first one to the list of
-                                                                                   // needed courses
+                            } else { // If there are multiple prerequisites
+                                // Check if any of the prerequisites have been completed
+                                boolean anyPrereqCompleted = false;
+                                for (String prereq : prereqList) {
+                                    if (compCourses.contains(allCourses.get(prereq))) {
+                                        anyPrereqCompleted = true;
+                                        break;
+                                    }
+                                }
+                                // If none of the prerequisites have been completed
+                                if (!anyPrereqCompleted && !needToTake.contains(allCourses.get(prereqList.get(0)))) {
+                                    needToTake.add(allCourses.get(prereqList.get(0))); // Add the first one to the list of
+                                                                                    // needed courses
+                                }
                             }
                         }
                     }
-                }
 
+                }
             }
         }
 
@@ -102,9 +105,11 @@ public class Scheduler {
             ArrayList<Course> semesterCourses = new ArrayList<>(); // Courses to take in the upcoming semester
             for (Iterator<Course> it = requiredCourses.iterator(); it.hasNext();) {
                 Course course = it.next();
-                if (isPrerequisiteCompleted(course.getPrereq(), allEligibleCourses)) {
-                    semesterCourses.add(course);
-                    it.remove(); // Remove the course as it's now scheduled
+                if (course != null) { // Check if the course is not null
+                    if (isPrerequisiteCompleted(course.getPrereq(), allEligibleCourses)) {
+                        semesterCourses.add(course);
+                        it.remove(); // Remove the course as it's now scheduled
+                    }
                 }
             }
             allEligibleCourses.addAll(semesterCourses); // Update the list of eligible courses
