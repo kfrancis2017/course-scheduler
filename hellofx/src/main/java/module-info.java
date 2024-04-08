51cf584a009aa30ec949cf2365f6cccf09263f1b
j-vaught <jvaught@sc.edu>
2024-04-08 09:14:28 -0400

    added dependencies

--- hellofx/src/main/java/module-info.java
+++ hellofx/src/main/java/module-info.java
@@ -1,7 +1,14 @@
 module org.openjfx.hellofx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
     opens org.openjfx.controllers to javafx.fxml;
     exports org.openjfx.controllers;
+
+    opens library to javafx.fxml;
+    exports library;
+
+    opens degreeswork to javafx.fxml;
+    exports degreeswork;
 }
