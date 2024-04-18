a58da5527b7b7c0dd0a282befcb2a2df9d7661db
JoshArega <112011105+JoshArega@users.noreply.github.com>
2024-04-18 14:34:47 -0400

    Push

---
+++ dgworks/src/main/java/controllers/Advisor8SemController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import dgworks.App;
+import model.*;
+
+public class Advisor8SemController {
+
+    @FXML
+    public void viewNotes(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void viewPlans(MouseEvent event) {
+
+    }
+
+}
