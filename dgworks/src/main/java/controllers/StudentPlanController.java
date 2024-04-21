2192cde2bb713ee04798d712896c6ee664f0c2da
JoshArega <112011105+JoshArega@users.noreply.github.com>
2024-04-21 15:10:31 -0400

    Student Plan Controller skeleton created

--- dgworks/src/main/java/controllers/StudentPlanController.java
+++ dgworks/src/main/java/controllers/StudentPlanController.java
@@ -5,11 +5,44 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import dgworks.App;
 import model.*;
 
 public class StudentPlanController implements Initializable {
+
+    @FXML
+    public Label lbl_description;
+
+    @FXML
+    public GridPane sem1;
+
+    @FXML
+    public GridPane sem2;
+
+    @FXML
+    public GridPane sem3;
+
+    @FXML
+    public GridPane sem4;
+
+    @FXML
+    public GridPane sem5;
+
+    @FXML
+    public GridPane sem6;
+
+    @FXML
+    public GridPane sem7;
+
+    @FXML
+    public GridPane sem8;
+
+    @FXML
+    public TextField txt_course;
 
     DegreeWorks dg = DegreeWorks.getInstance();
 
@@ -27,6 +60,16 @@ public class StudentPlanController implements Initializable {
     @FXML
     public void viewNotes(MouseEvent event) throws IOException {
         App.setRoot("student_notes");
+    }
+
+    @FXML
+    public void btnAdd(MouseEvent event) throws IOException {
+
+    }
+
+    @FXML
+    public void btnGenPlan(MouseEvent event) throws IOException {
+        
     }
     
     @Override
