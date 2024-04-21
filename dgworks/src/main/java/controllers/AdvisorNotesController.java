bf7a848899708dfabd0f3c843f8a987df8dd9f5d
JoshArega <112011105+JoshArega@users.noreply.github.com>
2024-04-21 14:36:40 -0400

    Advisor Notes works

--- dgworks/src/main/java/controllers/AdvisorNotesController.java
+++ dgworks/src/main/java/controllers/AdvisorNotesController.java
@@ -2,13 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
